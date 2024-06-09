36d07ee7e592854f63dc43ba05d431021e7b32f8
Erik Golinelli <erik@codekraft.it>
2024-06-09 12:23:50 +0200

    the block initialization

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockType } from '@wordpress/blocks';
+import { Button } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import metadata from './block.json';
+
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+registerBlockType(metadata.name, {
+	/**
+	 * @see ./edit.js
+	 */
+	edit: () => <Button>PDF</Button>,
+	/**
+	 * @see ./save.js
+	 */
+	save: () => <Button>PDF</Button>,
+});
